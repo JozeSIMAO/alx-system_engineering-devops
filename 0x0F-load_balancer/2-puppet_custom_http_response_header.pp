fc1acf926c991a522d121373a62fce45ef2b96fd
JozeSIMAO <jozesimao10@gmail.com>
2023-12-04 14:48:55 +0200

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,21 +8,49 @@
 #+ brand new Ubuntu machine to the requirements asked in this task
 
 # Install Nginx
-class { 'nginx':
-  manage_repo => true,
+package { 'nginx':
+  ensure => installed,
 }
 
-# Define a custom HTTP header
-nginx::resource::location { 'custom_header':
-  location      => '~ ^/',
-  server        => 'default',
-  set_raw       => ['add_header X-Served-By $hostname;'],
-  ssl           => false, # Set to true if you want to configure for HTTPS
+file { '/var/www/html':
+  ensure => directory,
 }
 
-# Restart Nginx to apply changes
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 https://youtube.com/;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ',
+}
+
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   enable  => true,
-  require => Class['nginx'],
+  require => File['/etc/nginx/sites-available/default'],
 }
