71532d585e06d585def6b1fd9a164e4461ab3811
JozeSIMAO <jozesimao10@gmail.com>
2024-01-03 00:39:53 +0200

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,19 +7,24 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    user_id = argv[1]
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_response = requests.get("{}/users/{}".format(base_url, user_id))
-    todos_response = requests.get("{}/todos?userId={}".
-                                  format(base_url, user_id))
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: {} <employee_id>".format(argv[0]))
+    else:
+        employee_id = int(argv[1])
+        base_url = "https://jsonplaceholder.typicode.com"
+        
+        user_response = requests.get(f"{base_url}/users/{employee_id}")
+        user_data = user_response.json()
+        employee_name = user_data.get("name")
 
-    user_data = user_response.json()
-    todos_data = todos_response.json()
+        todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
+        todo_data = todo_response.json()
 
-    completed_tasks = [task for task in todos_data if task['completed']]
-    total_tasks = len(todos_data)
+        total_tasks = len(todo_data)
+        completed_tasks = sum(task['completed'] for task in todo_data)
 
-    print("Employee {} is done with tasks({}/{})".
-          format(user_data['name'], len(completed_tasks), total_tasks))
-    for task in completed_tasks:
-        print("\t{}".format(task['title']))
+        print("Employee {} is done with tasks({}/{}):".
+              format(employee_name, completed_tasks, total_tasks))
+        for task in todo_data:
+            if task['completed']:
+                print("\t{}".format(task['title']))
