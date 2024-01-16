1c16c36ba17da24d9c6ac05d7fd95483e610ef27
JozeSIMAO <jozesimao10@gmail.com>
2024-01-16 21:24:27 +0200

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,24 +6,20 @@ the first 10 hot posts listed for a given subreddit.
 import requests
 from sys import argv
 
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    params = {'limit': 10}
-    response = requests.get(url, headers=headers,
-                            params=params, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json().get('data', {}).get('children', [])
-        for post in data:
-            print(post.get('data', {}).get('title'))
-    else:
+def top_ten(subreddit):
+    '''
+        returns the top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
         print(None)
 
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+if __name__ == "__main__":
+    top_ten(argv[1])
